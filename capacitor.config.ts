9a355e2688e588c97223304d2509fb7ac7148f1e
podum <patrick.odum1@gmail.com>
2022-11-24 08:08:16 +0000

    Rebrand

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'proj1',
+  appId: 'app.web.putadex',
+  appName: 'putadex',
   webDir: 'www',
   bundledWebRuntime: false
 };
